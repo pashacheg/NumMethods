9b02fcc5f7dabadb4b21fa69ab940e27cc8697f4
pashacheg <116956485+pashacheg@users.noreply.github.com>
2025-05-24 12:39:29 -0700

    Update running.m

--- kursovaya/running.m
+++ kursovaya/running.m
@@ -1,34 +1,33 @@
-function X=running(Y)
-  X = [0 0 0 0 0]';
-  M = [4 1 0 0 0; 3 6 1 0 0; 0 3 4 2 0; 0 0 1 5 3; 0 0 0 3 4];
+function X=running(M, Y, n)
+  X = zeros(n,1);
   
-  AL = [0 0 0 0 0];
-  BT = [0 0 0 0 0];
+  AL = [];
+  BT = [];
   
-  for i = 1 : 5
+  for i = 1 : n
     a = b = c = 0;
     al = bl = 0;
     
     b = M(i,i);
     if i == 1
       c = M(i,i+1);
-    elseif i == 5
+    elseif i == n
       a = M(i,i-1);
       al = AL(i-1);
-      bl = BL(i-1);
+      bl = BT(i-1);
     else
       a = M(i,i-1);
       c = M(i,i+1);
       al = AL(i-1);
-      bl = BL(i-1);
+      bl = BT(i-1);
     endif
     
-    AL(i) = -c / (b + a * al);
-    BL(i) = (Y(i) - a * bl) / (b + a * al);
+    AL = [AL -c/(b + a * al)];
+    BT = [BT (Y(i) - a * bl)/(b + a * al)];
   endfor
   
-  X(5) = BL(5);
-  for i = 4 : -1 : 1
-    X(i) = AL(i) * X(i+1)  + BL(i);
+  X(n) = BT(n);
+  for i = n-1 : -1 : 1
+    X(i) = AL(i) * X(i+1)  + BT(i);
   endfor
 end
